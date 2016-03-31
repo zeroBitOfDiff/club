d45fad7258e5d846b6e019255d8707b50ba3c22b
Jonathan Warner <jaxbot@gmail.com>
2016-03-31 19:24:30 -0400

    Add this thing

---
+++ pr.py
@@ -0,0 +1,16 @@
+#!/bin/python
+import subprocess
+
+branch = subprocess.check_output(["git rev-parse --abbrev-ref HEAD"], stderr=subprocess.STDOUT, shell=True)
+
+remote_url = subprocess.check_output(["git config --get remote.origin.url"], stderr=subprocess.STDOUT, shell=True)
+
+remote_url = remote_url.split("github.com:")
+remote_url = remote_url[1].split(".git")
+remote_url = remote_url[0]
+
+print remote_url
+
+url = "https://github.com/" + remote_url + "/compare/" + branch + "?expand=1"
+
+subprocess.check_output(["open " + url], stderr=subprocess.STDOUT, shell=True)
